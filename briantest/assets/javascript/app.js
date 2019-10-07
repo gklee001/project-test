2dbc2c4cbf7ce2cc7064e1dec280fd050777658f
gklee001 <53488991+gklee001@users.noreply.github.com>
2019-10-06 19:54:03 -0700

    Add files via upload

---
+++ briantest/assets/javascript/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    var take;
+
+
+
+    $("button").on("click", function () {
+        take = $('input').val();
+        $.ajax({
+            url: "http://api.edamam.com/search?q=" + take + "&app_id=$ba455ead&app_key=$31dcb7291ec3ec4dd89cd608adf97e57&from=0&to=1"
+            ,
+
+            method: "GET"
+        })
+            //we use response.data to move through the objects to grab the actual value
+            .then(function (response) {
+                var results = response.data;
+
+
+                console.log(response.hits[0].recipe.url);
+                console.log(response);
+                $('iframe').attr('src', response.hits[0].recipe.url)
+
+
+            })
+
+        $.ajax({
+            url: "https://api.unsplash.com/search/photos/?client_id=5f075f2a36d998d71e48a195d5b190a4c0b4194471f1a8108f42370aa300ce04&page=1&query=" + take,
+            method: "GET"
+        })
+
+            .then(function (dick) {
+                console.log(dick);
+                console.log(dick.results[0].urls.full);
+                $("img").attr("src", dick.results[0].urls.full);
+            }
+            )
+        console.log(take);
+    })
+
+
+
+
+}
+)
---
+++ briantest/assets/javascript/const.js
@@ -0,0 +1,1 @@
+var api = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
